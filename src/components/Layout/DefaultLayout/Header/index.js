58cde7ae5ee79d24849b0937565e7b5c67490ac7
hhoai <hhoaii261@gmail.com>
2024-05-01 16:55:48 +0700

    header

--- src/components/Layout/DefaultLayout/Header/index.js
+++ src/components/Layout/DefaultLayout/Header/index.js
@@ -1,5 +1,43 @@
+import { NavLink } from 'react-router-dom';
+
+import { AiOutlineHome, AiOutlineSearch } from 'react-icons/ai';
+
 function Header() {
-    return <h2>header</h2>;
+  return (
+    <div className="">
+      <div className="flex flex-col py-2 px-3 w-[420px] bg-[var(--background-color)] border border-[var(--background-color)] rounded-[8px]">
+        <div className="py-1 px-3">
+          <img className="w-[78px]" src="./logo1.png" alt="logo" />
+        </div>
+        <NavLink
+          className="flex items-center gap-2 py-1 px-3 font-semibold"
+          to="/"
+          style={({ isActive, isPending, isTransitioning }) => {
+            return {
+              color: isActive ? '#fff' : '#b3b3b3',
+              viewTransitionName: isTransitioning ? 'slide' : '',
+            };
+          }}
+        >
+          <AiOutlineHome className="text-2xl" />
+          Home
+        </NavLink>
+        <NavLink
+          className="flex items-center gap-2 py-1 px-3 font-semibold"
+          to="/search"
+          style={({ isActive, isPending, isTransitioning }) => {
+            return {
+              color: isActive ? '#fff' : '#b3b3b3',
+              viewTransitionName: isTransitioning ? 'slide' : '',
+            };
+          }}
+        >
+          <AiOutlineSearch className="text-2xl" />
+          Search
+        </NavLink>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -2,15 +2,17 @@ import Header from './Header';
 import Sidebar from './Sidebar';
 
 function defaultLayout({ children }) {
-    return (
-        <div>
-            <Header />
-            <div className="container">
-                <Sidebar />
-                <div className="content">{children}</div>
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex gap-2 pt-2 pl-2 bg-[var(--black)]">
+      <div className="flex flex-col gap-2">
+        <Header />
+        <Sidebar />
+      </div>
+      <div className="container">
+        <div className="content">{children}</div>
+      </div>
+    </div>
+  );
 }
 
 export default defaultLayout;
